d05b3beef826cabe26266fb64c22268513693a81
Ilia Lesik <ilialesik@gmail.com>
2020-03-12 14:54:23 +0300

    Refactor: create /transformers folder

---
+++ src/transformers/snapshotToReactFigmaAst.ts
@@ -0,0 +1,48 @@
+import * as t from "@babel/types";
+
+export const snapshotToReactFigmaAst = (snapshot) => {
+    const ComponentNode = t.exportNamedDeclaration(
+        t.variableDeclaration(
+            "const",
+            [t.variableDeclarator(
+                t.identifier("Component"),
+                t.arrowFunctionExpression(
+                    [],
+                    t.blockStatement([
+                        t.returnStatement(
+                            t.jsxElement(
+                                t.jsxOpeningElement(
+                                    t.jsxIdentifier(ElementToComponent[snapshot.type]),
+                                    convertToAttributes(snapshot),
+                                    true
+                                ),
+                                null,
+                                [],
+                                null
+                            )
+                        )
+                    ])
+                )
+            )]
+        )
+    );
+
+    return t.program([
+        ComponentNode
+    ]);
+};
+
+const ElementToComponent = {
+    "COMPONENT": "Component"
+};
+
+const convertToAttributes = (snapshot) => {
+    return Object.keys(snapshot)
+        .filter((key) => key !== "type" && key !== "children")
+        .map((key) => t.jsxAttribute(
+            t.jsxIdentifier(key),
+            typeof snapshot[key] === "number" && t.jsxExpressionContainer(
+            t.numericLiteral(snapshot[key])
+            )
+        ))
+};
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,57 +1,12 @@
 import generate from '@babel/generator';
-import * as t from "@babel/types";
-
-const ElementToComponent = {
-    "COMPONENT": "Component"
-};
+import {snapshotToReactFigmaAst} from "./transformers/snapshotToReactFigmaAst";
 
 onmessage = event => {
     console.log(event);
 
     const root = event.data && event.data.pluginMessage;
-    const output = generate(snapshotToAst(root));
+    const output = generate(snapshotToReactFigmaAst(root));
     console.log(output.code);
 };
 
-const convertToAttributes = (snapshot) => {
-    return Object.keys(snapshot)
-        .filter((key) => key !== "type" && key !== "children")
-        .map((key) => t.jsxAttribute(
-            t.jsxIdentifier(key),
-            typeof snapshot[key] === "number" && t.jsxExpressionContainer(
-                t.numericLiteral(snapshot[key])
-            )
-        ))
-};
 
-const snapshotToAst = (snapshot) => {
-    const ComponentNode = t.exportNamedDeclaration(
-        t.variableDeclaration(
-            "const",
-            [t.variableDeclarator(
-                t.identifier("Component"),
-                t.arrowFunctionExpression(
-                    [],
-                    t.blockStatement([
-                        t.returnStatement(
-                            t.jsxElement(
-                                t.jsxOpeningElement(
-                                    t.jsxIdentifier(ElementToComponent[snapshot.type]),
-                                    convertToAttributes(snapshot),
-                                    true
-                                ),
-                                null,
-                                [],
-                                null
-                            )
-                        )
-                    ])
-                )
-            )]
-        )
-    );
-
-    return t.program([
-        ComponentNode
-    ]);
-};
